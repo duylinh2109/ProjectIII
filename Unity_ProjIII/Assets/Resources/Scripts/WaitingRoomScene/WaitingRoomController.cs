126fd829438d9ba9c2fcdec65afca61b30063ac7
fmo080308 <fmo080308@hotmail.com>
2015-11-17 08:17:54 +0700

    Edit script

---
+++ Unity_ProjIII/Assets/Resources/Scripts/WaitingRoomScene/WaitingRoomController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitingRoomController : MonoBehaviour
+{
+    public GameObject exitLobby;
+    public GameObject createRoom;
+
+    public GameObject smartFoxLobby;
+
+    // Hàm chọn exit lobby
+    public void clickExitLobby()
+    {
+        Application.Quit();
+        Debug.Log("Out Lobby");
+    }
+
+    //Hàm chọn Create Room
+    public void clickCreateRoom()
+    {
+        smartFoxLobby.GetComponent<SmartFoxWaitingRoom>().createRoom();
+        //Application.LoadLevel("MainRoom");
+    }
+
+    public void clickSendMessage()
+    {
+        smartFoxLobby.GetComponent<SmartFoxWaitingRoom>().SendPublicMessage();
+    }
+
+    public void clickJoinRoom()
+    {
+        GameObject.Find("SmartFoxWaitingRoomController").GetComponent<SmartFoxWaitingRoom>().JoinRoom(this.transform.Find("Text").GetComponent<Text>().text);
+    }
+}
